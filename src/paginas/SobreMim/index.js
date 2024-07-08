c835598529e1fd2df4d8f03db13733a5a5274726
DavidAndradee <david1999andrade@gmail.com>
2024-07-08 12:10:55 -0300

    ajusts

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -8,16 +8,16 @@ import fotoSobreMim from "assets/sobre_mim_foto.jpeg"
 export default function SobreMim() {
     return (
         <PostModelo fotoCapa={fotoCapa} titulo="Sobre Mim">
-            <h3 className={styles.subtitulo}>Olá DAvid na Area</h3>
+            <h3 className={styles.subtitulo}>Olá, um pouco sobre mim.</h3>
             <img src={fotoSobreMim} className={styles.fotoSobreMim} />
             <p className={styles.paragrafo}>
                 Engenheiro de Software com domínio no processo de desenvolvimento de sistemas de maneira ágil. Focado em Qualidade de software e do processo de desenvolvimento, com enfâse em testes, verificação e validação funcional e back-end voltada a requisições de API.
             </p>
             <p className={styles.paragrafo}>
-                Experiência e conhecimentos solido na análise de Requisitos e de Sistemas, crucial para garantir que o produto final atenda às expectativas dos usuários e dos stakeholders.
+                Conhecimentos solido e experiência na análise de Requisitos e de Sistemas, crucial para garantir que o produto final atenda às expectativas dos usuários e dos stakeholders.
             </p>
             <p className={styles.paragrafo}>
-                Experiência avançada em desenvolvimento ágil, demonstrando capacidade de adaptação a mudanças e colaboração proativa e eficaz em equipes multifuncionais. Profundo entendimento dos requisitos do projeto, demonstrando habilidade para analisar e interpretar detalhadamente as necessidades do cliente.
+                Experiência avançada em desenvolvimento ágil, com capacidade de adaptação a mudanças e colaboração proativa e eficaz em equipes multifuncionais. Profundo entendimento dos requisitos do projeto, demonstrando habilidade para analisar e interpretar detalhadamente as necessidades do cliente.
             </p>
             <p className={styles.paragrafo}>
                 Experiência em prototipação, destacando a capacidade de criar modelos visuais e funcionais para validar conceitos e antecipar potenciais desafios. Competências recentemente adquiridas em desenvolvimento front-end, fortalecendo a capacidade de análise para garantia da qualidade, ao entender os aspectos técnicos e estéticos da interface do usuário.
